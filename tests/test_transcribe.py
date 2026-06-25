04559ad21a1a32cecf5af8150219175167931b4a
ol1xy <aleksejserduk1@gmail.com>
2026-06-26 02:17:23 +0800

    test: written mocked test for whisper local adapter

---
+++ tests/test_transcribe.py
@@ -0,0 +1,26 @@
+from unittest.mock import MagicMock, patch
+import pytest
+from src.transcriptor_tracker.transcribe import WhisperLocalAdapter
+
+
+@patch("src.transcriptor_tracker.transcribe.WhisperModel")
+def test_whisper_local_adapter_success(mock_whisper_model):
+
+    mock_model_instance = MagicMock()
+    mock_whisper_model.return_value = mock_model_instance
+
+    mock_segment = MagicMock()
+    mock_segment.text = "Привет, это тестовый транскрипт"
+
+    mock_model_instance.transcribe.returm_value = ([mock_segment], None)
+
+    adapter = WhisperLocalAdapter(model_size="tiny")
+    result = adapter.transcribe("data/examples/sample_meeting.mp3")
+
+    assert result == "Привет, это тестовый транскрипт"
+
+    mock_whisper_model.assert_called_once_with("tiny", device = "cpu",
+                                               compute_type = "int8")
+    
+    mock_model_instance.transcribe.assert_called_once_with("data/examples/sample_meeting.mp3",
+                                                           beam_size = 5)
